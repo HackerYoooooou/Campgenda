06172a0171be486c0f89831e1da20a0b1ced1f1e
Anastasiya <anasatsya.melenteva@gmail.com>
2018-02-14 17:08:24 -0500

    Set Up eBird API and ser up Firebase

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 const userInput = {};
 
+// Initializa Firebase
+userInput.initFirebase = () => {
+    var config = {
+        apiKey: "AIzaSyDO9VDh2kcm-ob4BiOuG7BHUIoCZbLGEJ4",
+        authDomain: "campgenda.firebaseapp.com",
+        databaseURL: "https://campgenda.firebaseio.com",
+        projectId: "campgenda",
+        storageBucket: "campgenda.appspot.com",
+        messagingSenderId: "116728568896"
+    };
+    firebase.initializeApp(config);
+}
+
+
 // Retrieve User Input
-userInput.province = $('input[name=province]').val();
-userInput.city = $('input[name=city]').val();
-userInput.radius = $('input[name=radius]').val();
-userInput.activity = $('input[name=activity]').val();
-
-
-
+userInput.retrieveInputValues = function() {
+    userInput.province = $('input[name=province]').val();
+    userInput.city = $('input[name=city]').val();
+    userInput.radius = $('input[name=radius]').val();
+    userInput.activity = $('input[name=activity]').val();
+    userInput.getLocationBasedOnUserInput(userInput.province, userInput.city, userInput.radius, userInput.activity);
+}
 
 // Create AJAX request to retrieve location information based on City
-const getLocationBasedOnUserInput = (province, city, radius, activity) => {
+
+userInput.getLocationBasedOnUserInput = (province, city, radius, activity) => {
     return $.ajax({
         url: 'https://trailapi-trailapi.p.mashape.com/',
         method: 'GET',
@@ -26,29 +41,90 @@ const getLocationBasedOnUserInput = (province, city, radius, activity) => {
                 activities_activity_type_name_eq: activity
             }
         }
+    }).then(function(res){
+        const data = res.places;
+        userInput.displayOptions(data);
     });
 }
 
-async function getData() {
-    const returnedLocations = await getLocationBasedOnUserInput(userInput.province, userInput.city, userInput.radius, userInput.activity);
-    console.log(returnedLocations.places);
-    // return returnedLocations;
-    returnedLocations.places.forEach((location) => {
-        console.log(location.name);
-    });
-}
-
-// userInput.displayOptions = () => {
-//     let returnedLocations = getData();
-//     returnedLocations.forEach((location) => {
-//         console.log(location.name);
-//     });
+// Create async function -DISREGARD (NOT WORKING/ HERE FOR FUTURE REFERENCE)
+// to wait for the promise and call display function when promise is recieved
+// userInput.getLocationsData = () => {
+    // async function getData() {
+    //     userInput.retrieveInputValues(); 
+    //     const returnedLocations = await userInput.getLocationBasedOnUserInput(userInput.province, userInput.city, userInput.radius, userInput.activity);
+    //     console.log(returnedLocations.places);
+    //     userInput.displayOptions(returnedLocations.places);
+    // }
 // }
 
+// Create function local to userInput object
+// to print attributes of each element of locations array on the screen
+userInput.displayOptions = (locations) => {
+    locations.forEach((location) => {
+        console.log(location.name);
+        const name = $('<h2>').text(location.name);
+        const direction = $('<p>').text(location.directions);
+        const container = $('<div>').append(name, direction);
+        $('body').append(container);
+    });
+}
+
+
+
+// Create function to retrieve location code from firebase based on user input
+userInput.retrieveLocationCode = function() {
+    const dbRef = firebase.database().ref('/codes');
+    dbRef.on('value', (item) => {
+        const codeObject = item.val();
+        for (let key in codeObject) {
+            console.log(codeObject[key]);
+            if (codeObject[key]["country-code"] === 'CA' && codeObject[key]["name"] === userInput.city ) {
+                    console.log(codeObject[key]);
+                }
+        }
+    });
+}
+    // && codeObject[key].name == userInput.city
+
+// BIRDS API
+const getBirdsBasedOnLocation = (sightSpot) => {
+    return $.ajax({
+        url: 'http://ebird.org/ws1.1/data/notable/region/recent',
+        // url: 'https://proxy.hackeryou.com',
+        method: 'GET',
+        dataType: 'jsonp',
+        headers: {
+            'X-eBirdApiToken': 'ibcn8ifsm3dq'
+        },
+        data: {
+            r: sightSpot,
+            fmt: 'json'
+        }
+    });
+}
+
+let BirdLoc = 'CA-ON-TO';
+async function getDataAboutBirds() {
+    const birds = await getBirdsBasedOnLocation(BirdLoc);
+    console.log(`Here are birdies: ${birds}`);
+}
+
+
+userInput.init = function () {
+    
+}
+
 $(function(){
+    // userInput.init();
+    userInput.initFirebase();
+    // userInput.retrieveLocationCode();
     $('form').on('submit', function(event){
+        
         event.preventDefault();
-        getData();
-        // userInput.displayOptions();
+        // getData();
+        userInput.retrieveInputValues();
+        userInput.retrieveLocationCode();
+        getDataAboutBirds();
     })
 });
